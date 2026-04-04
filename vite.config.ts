3169f6ed39ad2d958df8e0b3e75d98d5acdfd408
toidputS <klutchkanobi@gmail.com>
2026-04-04 12:55:29 -0400

    Fix corrupted index.html, remove tailwind CDN script

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["icons/favicon.svg", "apple-touch-icon.png", "masked-icon.svg"],
+      includeAssets: ["icons/favicon.svg"],
       manifest: {
         name: "Club Youniverse Live",
         short_name: "Youniverse",
@@ -19,20 +20,14 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icons/icon-192.png",
+            src: "/icons/icon-192.svg",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/svg+xml"
           },
           {
-            src: "/icons/icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable"
-          },
-          {
-            src: "https://ktfezfnkghtwbkmhxdyd.supabase.co/storage/v1/object/public/site_assets/icon-512.png",
+            src: "/icons/icon-192.svg",
             sizes: "512x512",
-            type: "image/png"
+            type: "image/svg+xml"
           }
         ]
       },
